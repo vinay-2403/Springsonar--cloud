91ca69412159b9632fec9cf93d78380fb5ce35da
vinay <vinay.madanambeti@zemosolabs.com>
2025-07-01 13:46:09 +0530

    feat-Added all the files of Loggers

---
+++ src/main/java/org/example/loggerproduct/model/Product.java
@@ -0,0 +1,16 @@
+package org.example.loggerproduct.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+}
